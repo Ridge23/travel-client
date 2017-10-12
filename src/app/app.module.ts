e9e945159de23f1723b5a017caa226645cdee3c5
Pavel Khrebto <rezgo.dev@dataart.com>
2017-10-12 11:22:08 +0200

    add selected part component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule }   from '@angular/forms'
 
 import { MainComponent } from './components/main/main.component';
 import { SearchFormComponent } from './components/search-form/search-form.component';
-import { RightColumnCard } from './components/main/right-column-card.component';
-import { SelectedObjectCard } from './components/main/selected-object-card.component';
+import { RightColumnCard } from './components/cards/right-column-card.component';
+import { SelectedPartCard } from './components/cards/selected-part-card.component';
+import { SelectedObjectCard } from './components/cards/selected-object-card.component';
+
 import { IziTravelService } from './services/izi-travel.service';
 import { EscapeHtmlPipe } from './pipes/keep-html.pipe';
+
 
 @NgModule({
   declarations: [
@@ -15,7 +18,8 @@ import { EscapeHtmlPipe } from './pipes/keep-html.pipe';
     SearchFormComponent,
     RightColumnCard,
     EscapeHtmlPipe,
-    SelectedObjectCard
+    SelectedObjectCard,
+    SelectedPartCard
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/cards/selected-part-card.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'selected-part-card',
+    templateUrl: './selected-part-card.component.html',
+    styleUrls: ['./selected-part-card.component.css']
+})
+export class SelectedPartCard {
+    @Input() selectedPart;
+
+    /**
+     * @param {string} contentProviderUUId 
+     * @param {string} audioUUId 
+     * 
+     * @return {string}
+     */
+    getMediaUrl() {
+        console.log(this.selectedPart.content[0].audio[0].uuid );
+        return 'https://media.izi.travel/' 
+            + this.selectedPart.content_provider.uuid 
+            + '/' 
+            + this.selectedPart.content[0].audio[0].uuid 
+            + '.m4a';
+    }
+}
